522d008dde6d6f0f88b59d8d26a74efecc7b699b
Ander Garmendia <agarmendia@countercraft.eu>
2017-07-29 23:43:56 +0200

    Added simple routing

--- src/app/estropadak-list/estropadak-list.component.ts
+++ src/app/estropadak-list/estropadak-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { EstropadaService } from '../shared/estropada.service';
 
 @Component({
@@ -6,14 +7,24 @@ import { EstropadaService } from '../shared/estropada.service';
   templateUrl: './estropadak-list.component.html',
   styleUrls: ['./estropadak-list.component.css']
 })
-export class EstropadakListComponent {
+export class EstropadakListComponent implements OnInit {
 
   estropadak: any = [];
-  constructor(private estropadaService: EstropadaService) { 
+  constructor(
+    private estropadaService: EstropadaService,
+    private router: Router
+  ) { }
+
+  ngOnInit(){
     this.updateEstropadak();
   }
-
+  
   updateEstropadak(){
     this.estropadaService.getList().subscribe((estropadak) => this.estropadak = estropadak);
+  }
+
+  onSelect(estropada){
+    console.log(estropada.name + 'Selected');
+    this.router.navigate(['/estropada', estropada.id]);
   }
 }
