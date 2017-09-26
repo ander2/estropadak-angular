79561387f132543055ae71ab5bda37049516d206
Ander Garmendia <kelertxiki@gmail.com>
2017-09-26 23:10:25 +0200

    Added material design table

--- src/app/estropada-tanda/estropada-tanda.component.ts
+++ src/app/estropada-tanda/estropada-tanda.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {DataSource} from '@angular/cdk/collections';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-estropada-tanda',
@@ -9,10 +13,27 @@ export class EstropadaTandaComponent implements OnInit {
 
   @Input()
   tanda;
+  displayedColumns = ['Postua', 'Taldea', 'Kalea', '1.z',
+  '2.z', '3.z', 'Denbora'];
+  dataSource;
   constructor() { }
 
   ngOnInit() {
-    console.log(this.tanda.tanda);
+    this.dataSource = new EstropadaDataSource(this.tanda);
   }
 
+}
+
+class EstropadaDataSource extends DataSource<any>{
+    tanda;
+    constructor(tanda) {
+    super();
+    this.tanda = tanda;
+    }
+
+    connect(): Observable<any> {
+        return Observable.of(this.tanda);
+    }
+
+    disconnect(){}
 }
