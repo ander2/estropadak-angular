0752061bc1f92ce4626d8436058bf875225a7fd1
Ander Garmendia <kelertxiki@gmail.com>
2017-12-17 17:17:26 +0100

    Added stats for rank and wins

--- src/app/estropadak-stats/estropadak-stats.component.ts
+++ src/app/estropadak-stats/estropadak-stats.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, ViewEncapsulation } from '@angular/core';
 import { EstropadaService } from 'app/shared/estropada.service';
 import 'nvd3';
 declare let d3: any;
@@ -11,46 +11,72 @@ declare let d3: any;
   ],
   encapsulation: ViewEncapsulation.None
 })
-export class EstropadakStatsComponent implements OnChanges {
+export class EstropadakStatsComponent implements OnInit, OnChanges {
   options;
+  piechartOptions;
   data;
   estropadak = [];
   cumulative = [];
+  rank = [];
+  wins = [];
 
   @Input() year;
   @Input() league;
 
   constructor(private estropadaService: EstropadaService) { }
 
+  ngOnInit() {
+    this.options = {
+      chart: {
+        type: 'lineChart',
+        height: 450,
+        margin : {
+          top: 20,
+          right: 20,
+          bottom: 50,
+          left: 55
+        },
+        x: (d) => d.label,
+        y: (d) => d.value,
+        xAxis: {
+          axisLabel: 'Estropadak',
+          tickFormat: (d) => this.estropadak[d],
+          rotateLabels: 25,
+          showMaxMin: false
+        },
+        yAxis: {
+          axisLabel: 'Puntuak',
+        },
+      }
+    };
+    this.piechartOptions = {
+      chart: {
+        type: 'pieChart',
+        height: 450,
+        margin : {
+          top: 20,
+          right: 20,
+          bottom: 50,
+          left: 55
+        },
+        x: (d) => d.label,
+        y: (d) => d.value,
+        showValues: true
+      }
+    };
+  }
+
   ngOnChanges() {
+    if (this.league === null || this.league === undefined) {
+      this.league = 'ACT';
+    }
+    if (this.year === null || this.year === undefined) {
+      this.year = '2017';
+    }
     this.estropadaService.getList(this.league, this.year)
     .subscribe((estropadak) => {
       this.estropadak = estropadak.map((estropada) => estropada.key[2])
       .filter((estropada) => estropada.indexOf('Play') === -1)
-      console.log(this.estropadak);
-      this.options = {
-        chart: {
-          type: 'lineChart',
-          height: 450,
-          margin : {
-            top: 20,
-            right: 20,
-            bottom: 50,
-            left: 55
-          },
-          x: (d) => d.label,
-          y: (d) => d.value,
-          xAxis: {
-            axisLabel: 'Estropadak',
-            tickFormat: (d) => this.estropadak[d],
-            rotateLabels: 25,
-            showMaxMin: false
-          },
-          yAxis: {
-            axisLabel: 'Puntuak',
-          },
-        }
-      };
       this.estropadaService.getOne(`rank_${this.league.toUpperCase()}_${this.year}`)
       .subscribe((res) => {
         const stats = res.stats;
@@ -77,7 +103,21 @@ export class EstropadakStatsComponent implements OnChanges {
             values: values
           }
         });
-        console.log(this.cumulative);
+        this.rank = Object.keys(stats).map((teamName) => {
+          return {
+            label: teamName,
+            value: stats[teamName].points
+          };
+        });
+        this.wins = Object.keys(stats).reduce((memo, teamName) => {
+          if (stats[teamName].wins > 0) {
+            memo.push({
+              label: teamName,
+              value: stats[teamName].wins
+            });
+          }
+          return memo;
+        }, []);
       });
     });
   }
