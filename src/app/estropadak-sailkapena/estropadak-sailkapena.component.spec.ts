66c6e587ada6f0d3a4888d5225333a6189c83dfb
Ander Garmendia <kelertxiki@gmail.com>
2018-03-11 21:31:57 +0100

    Fixed sailkapena tests

--- src/app/estropadak-sailkapena/estropadak-sailkapena.component.spec.ts
+++ src/app/estropadak-sailkapena/estropadak-sailkapena.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EstropadakSailkapenaComponent } from './estropadak-sailkapena.component';
+import { SailkapenaService } from '../shared/estropada.service';
+import { SailkapenaServiceStub } from '../shared/estropada.service.stub';
 
 describe('EstropadakSailkapenaComponent', () => {
   let component: EstropadakSailkapenaComponent;
@@ -8,7 +10,11 @@ describe('EstropadakSailkapenaComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EstropadakSailkapenaComponent ]
+      declarations: [ EstropadakSailkapenaComponent ],
+      providers: [
+        {provide: SailkapenaService, useClass: SailkapenaServiceStub}
+      ]
+
     })
     .compileComponents();
   }));
