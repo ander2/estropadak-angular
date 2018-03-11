e8ec2d8083c0c0133911f5cdb579ada18a53181f
Ander Garmendia <kelertxiki@gmail.com>
2018-03-11 21:31:13 +0100

    Fixed tanda tests

--- src/app/estropada-tanda/estropada-tanda.component.spec.ts
+++ src/app/estropada-tanda/estropada-tanda.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatTableModule } from '@angular/material';
 
 import { EstropadaTandaComponent } from './estropada-tanda.component';
+import { By } from '@angular/platform-browser';
+import { estropada } from '../shared/estropada.fixture';
 
 describe('EstropadaTandaComponent', () => {
   let component: EstropadaTandaComponent;
@@ -8,6 +13,10 @@ describe('EstropadaTandaComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MatTableModule,
+        FlexLayoutModule
+      ],
       declarations: [ EstropadaTandaComponent ]
     })
     .compileComponents();
@@ -16,10 +25,23 @@ describe('EstropadaTandaComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EstropadaTandaComponent);
     component = fixture.componentInstance;
+    component.tanda = estropada.sailkapena.filter(estrop => estrop.tanda === 1);
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show tanda number in title', () => {
+    expect(fixture.debugElement.query(By.css('h2')).nativeElement.innerHTML).toBe('1. tanda');
+  });
+
+  it('should show one row per team', () => {
+    expect(fixture.debugElement.queryAll(By.css('mat-row')).length).toEqual(4);
+  });
+
+  it('should have 7 columns per row(team)', () => {
+    expect(fixture.debugElement.queryAll(By.css('mat-row mat-cell')).length).toEqual(28);
   });
 });
