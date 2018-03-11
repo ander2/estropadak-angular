f73d4fa06d95b3440265349299bc400b5712461e
Ander Garmendia <kelertxiki@gmail.com>
2018-03-11 17:20:19 +0100

    Updated dependencies

--- src/app/estropada-detail/estropada-detail.component.spec.ts
+++ src/app/estropada-detail/estropada-detail.component.spec.ts
@@ -2,11 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EstropadaDetailComponent } from './estropada-detail.component';
 import { MatIconModule, MatTableModule } from '@angular/material';
+import {MatDividerModule } from '@angular/material/divider';
 import { EstropadaEstropadaSailkapenaComponent } from '../estropada-estropada-sailkapena/estropada-estropada-sailkapena.component';
 import { EstropadaTandaComponent } from '../estropada-tanda/estropada-tanda.component';
 import { EstropadaService } from '../shared/estropada.service';
 import { EstropadaServiceStub } from '../shared/estropada.service.stub';
-import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('EstropadaDetailComponent', () => {
@@ -17,6 +17,7 @@ describe('EstropadaDetailComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         MatIconModule,
+        MatDividerModule,
         MatTableModule,
         RouterTestingModule
       ],
