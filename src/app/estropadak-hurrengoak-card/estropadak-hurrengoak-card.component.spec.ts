ba309879e02b68648fd2889f25cdd10d69af0f5b
Ander Garmendia <kelertxiki@gmail.com>
2018-05-22 22:56:14 +0200

    Fix test

--- src/app/estropadak-hurrengoak-card/estropadak-hurrengoak-card.component.spec.ts
+++ src/app/estropadak-hurrengoak-card/estropadak-hurrengoak-card.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MatCardModule, MatListModule } from '@angular/material';
+
 import { EstropadakHurrengoakCardComponent } from './estropadak-hurrengoak-card.component';
+import { EstropadaServiceStub } from '../shared/estropada.service.stub';
+import { EstropadaService } from '../shared/estropada.service';
+import { EstropadakNavegationService } from '../shared/estropadak-navegation.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('EstropadakHurrengoakCardComponent', () => {
   let component: EstropadakHurrengoakCardComponent;
@@ -8,7 +14,16 @@ describe('EstropadakHurrengoakCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EstropadakHurrengoakCardComponent ]
+      imports: [
+        MatCardModule,
+        MatListModule,
+        RouterTestingModule
+      ],
+      declarations: [ EstropadakHurrengoakCardComponent ],
+      providers:[ 
+        { provide: EstropadaService, useClass: EstropadaServiceStub },
+        EstropadakNavegationService
+      ]
     })
     .compileComponents();
   }));
