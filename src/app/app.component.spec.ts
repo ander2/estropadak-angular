ecac3c43229fbfe589e33eddfae7a7bc21cb9dc7
Ander Garmendia <kelertxiki@gmail.com>
2018-08-15 12:08:44 +0200

    Fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,18 +2,34 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule, 
+  MatMenuModule} from '@angular/material';
+import { UrteakService } from './shared/estropada.service';
+import { UrteakServiceStub } from './shared/estropada.service.stub';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatIconModule,
+        MatMenuModule,
         RouterTestingModule.withRoutes(
           [{path: '', component: AppComponent}]
         )
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent
       ],
+      providers: [
+        {provide: UrteakService, useClass: UrteakServiceStub}
+      ]
     }).compileComponents();
   }));
 
