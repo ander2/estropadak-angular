b95ffc6d92dcba8ca8615d60a394ad77a6472ee7
Ander Garmendia <kelertxiki@gmail.com>
2018-11-10 16:16:42 +0100

    Fix chart bad resizing

--- src/app/estropadak-page/estropadak-page.component.ts
+++ src/app/estropadak-page/estropadak-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd } from '@angular/router';
+import { MatTabChangeEvent } from '@angular/material';
 
 @Component({
   selector: 'app-estropadak-page',
@@ -10,6 +11,7 @@ export class EstropadakPageComponent implements OnInit {
 
   league;
   year;
+
   constructor(
     private route: ActivatedRoute
   ) { }
@@ -19,5 +21,14 @@ export class EstropadakPageComponent implements OnInit {
       this.league = params.get('league');
       this.year = params.get('year');
     });
+  }
+
+  resizeIfStats(ev: MatTabChangeEvent) {
+    if (ev.index === 2) {
+      setTimeout(() => {
+        console.log('imeout');
+        window.dispatchEvent(new Event('resize'));
+      }, 300);
+    }
   }
 }
