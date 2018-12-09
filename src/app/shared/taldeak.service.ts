969ce78ef077171d22e3545b864a8a1293f51400
Ander Garmendia <kelertxiki@gmail.com>
2018-12-09 16:47:49 +0100

    Add support for team statistics

---
+++ src/app/shared/taldeak.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { environment } from '../../environments/environment';
+
+const estropadakUrl = environment.apiUrl;
+
+@Injectable()
+export class TaldeakService {
+
+  constructor(private http: Http) { }
+
+  getList(league?: string, year?: string): Observable<any> {
+      const endpoint = `${estropadakUrl}taldeak`;
+      return this.http.get(endpoint)
+          .map(res => res.json())
+  }
+}
