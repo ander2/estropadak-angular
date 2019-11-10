ede417f4d951cbed067aa3e72768bff0ff3e0d2e
Ander Garmendia <kelertxiki@gmail.com>
2019-11-10 16:00:06 +0100

    Add selection component

---
+++ src/app/estropadak-selection-form/estropadak-selection-form.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UrteakService } from 'app/shared/estropada.service';
+import { TaldeakService } from 'app/shared/taldeak.service';
+
+@Component({
+  selector: 'app-estropadak-selection-form',
+  templateUrl: './estropadak-selection-form.component.html',
+  styleUrls: ['./estropadak-selection-form.component.css']
+})
+export class EstropadakSelectionFormComponent implements OnInit {
+
+  @Input()
+  league: string;
+  @Input()
+  year: string;
+  @Input()
+  team: string;
+  @Output()
+  teamChanged: EventEmitter<any> = new EventEmitter();
+  form: FormGroup;
+  leagues: string[];
+  years: number[];
+  teams: string[] = [];
+  showYears = true;
+  showTeams = true;
+  allYears: {[key: string]: number[]};
+  constructor(
+    private fb: FormBuilder,
+    private yearService: UrteakService,
+    private taldeakService: TaldeakService,
+  ) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      league: [this.league, Validators.required],
+      year: [this.year, Validators.required],
+      team: [this.team, Validators.required]
+    });
+    this.yearService.getList().subscribe( years => {
+      this.allYears = years;
+      this.leagues = Object.keys(years).sort();
+      this.updateYears();
+    });
+    this.taldeakService.getList(this.league, this.year).subscribe(teams => {
+      this.teams = teams;
+    });
+  }
+
+  updateYears() {
+    const league = this.form.get('league').value;
+    const year = this.form.get('year').value;
+    this.years = this.allYears[league].sort((a, b) => b - a);
+  }
+
+  updateYearsAndRefresh() {
+    this.updateYears();
+    this.form.get('year').setValue(this.years[0]);
+  }
+
+  teamSelected() {
+    const values = this.form.getRawValue();
+    this.teamChanged.emit(values);
+  }
+
+  teamChange() {
+
+  }
+
+  yearChange() {
+    const year = this.form.get('year').value;
+    const league = this.form.get('league').value;
+
+    this.taldeakService.getList(league, year).subscribe(res => {
+      console.log(res);
+      this.teams = res;
+    });
+  }
+
+}
