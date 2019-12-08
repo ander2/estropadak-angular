530542d3c8d834c37b02b61db38580fa941f53fd
Ander Garmendia <kelertxiki@gmail.com>
2019-12-08 12:38:32 +0100

    First version that makes sense

--- src/app/estropadak-rower-graph/estropadak-rower-graph.component.ts
+++ src/app/estropadak-rower-graph/estropadak-rower-graph.component.ts
@@ -17,44 +17,53 @@ export class EstropadakRowerGraphComponent implements AfterViewInit{
 
     node_name: string;
 
-    // runLayout = function(){
-    //   var p = node.data('orgPos');
+    runLayout = function(node){
+      const p = node.data('orgPos');
 
-    //   var l = nhood.filter(':visible').makeLayout({
-    //     name: 'concentric',
-    //     fit: false,
-    //     animate: true,
-    //     animationDuration: aniDur,
-    //     animationEasing: easing,
-    //     boundingBox: {
-    //       x1: p.x - 1,
-    //       x2: p.x + 1,
-    //       y1: p.y - 1,
-    //       y2: p.y + 1
-    //     },
-    //     avoidOverlap: true,
-    //     concentric: function( ele ){
-    //       if( ele.same( node ) ){
-    //         return 2;
-    //       } else {
-    //         return 1;
-    //       }
-    //     },
-    //     levelWidth: function(){ return 1; },
-    //     padding: layoutPadding
-    //   });
+      // const l = // node.neighborhood().filter(':visible').layout({
+      const l = {
+        name: 'concentric',
+        fit: true,
+        animate: true,
+        animationDuration: 3,
+        // animationEasing: easing,
+        // boundingBox: {
+        //   x1: p.x - 1,
+        //   x2: p.x + 1,
+        //   y1: p.y - 1,
+        //   y2: p.y + 1
+        // },
+        // avoidOverlap: true,
+        concentric: function( ele ){
+          if( ele.data('team') ){
+            return 2;
+          } else {
+            return 1;
+          }
+        },
+        levelWidth: function(){ return 2; },
+        padding: 25 // layoutPadding
+      };
 
-    //   var promise = cy.promiseOn('layoutstop');
+      // var promise = cy.promiseOn('layoutstop');
 
-    //   l.run();
+      // l.run();
 
-    //   return promise;
-    // };
+      // return promise;
+      return l;
+    };
 
   layout = {
     name: 'concentric',
-    concentric: function (ele) {
-      return 1;
+    fit: true,
+    animate: true,
+    animationDuration: 10,
+    concentric: function( ele ){
+      if( ele.data('team') ){
+        return 2;
+      } else {
+        return 1;
+      }
     },
     levelWidth: function () { return 1; },
   };
@@ -83,7 +92,8 @@ export class EstropadakRowerGraphComponent implements AfterViewInit{
                 name: element.name,
                 weight: 100,
                 colorCode: 'green',
-                shapeType: 'ellipse'
+                shapeType: 'ellipse',
+                team: true
               }
             }
           }),
@@ -93,11 +103,11 @@ export class EstropadakRowerGraphComponent implements AfterViewInit{
   }
 
   nodeChange(event) {
-    const team = event;
+    const team = event.data('name');
     console.log('Team clicked', team);
     this.taldeakService.getOne(team, 'ACT', 2019)
       .subscribe(res => {
-        this.graphData = res.rowers.map(rower => {
+        const nodes = res.rowers.map(rower => {
           return {
             group: 'nodes',
             data: {
@@ -105,10 +115,30 @@ export class EstropadakRowerGraphComponent implements AfterViewInit{
               name: rower.name,
               weight: 100,
               colorCode: 'green',
-              shapeType: 'ellipse'
+              shapeType: 'ellipse',
+              rower: true
             }
           }
-        })
+        });
+
+        const edges = res.rowers.map(rower => {
+          return {
+            group: 'edges',
+            data: {
+              id: `${rower.name}_${team}`,
+              source: rower.name,
+              target: team,
+              strength: 1,
+              colorCode: 'blue'
+            }
+          }
+        });
+        this.graphData = {
+          nodes: nodes,
+          edges: edges
+        };
+        // const newLayout = this.runLayout(event)
+        // this.layout = newLayout;
       });
   }
 
