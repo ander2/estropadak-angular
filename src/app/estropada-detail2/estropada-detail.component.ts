a55b4cfee0ecae3290da2c8b5de8dab70b009aaf
Ander Garmendia <kelertxiki@gmail.com>
2020-01-16 23:25:35 +0100

    Initial changes to support batelak

---
+++ src/app/estropada-detail2/estropada-detail.component.ts
@@ -0,0 +1,68 @@
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { EstropadaService } from '../shared/estropada.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { EstropadaTandaComponent } from '../estropada-tanda/estropada-tanda.component';
+
+@Component({
+  selector: 'app-estropada-detail',
+  templateUrl: './estropada-detail.component.html',
+  styleUrls: ['./estropada-detail.component.css']
+})
+
+export class EstropadaDetailComponent implements OnInit {
+
+  estropada: any = {};
+  id = '1';
+  datasource;
+  constructor(
+    private estropadaService: EstropadaService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+      this.refresh();
+    });
+  }
+
+  refresh( ) {
+    this.estropadaService.getOne(this.id)
+    .subscribe((estropada) => {
+      const tandak = [];
+      const sailkapena = estropada.sailkapena || [];
+      // const tanda1 = sailkapena.filter((el) => el.tanda === 1);
+      sailkapena.forEach(sailk => {
+        const index = sailk.tanda - 1;
+        if (tandak[index]) {
+          tandak[index].push(sailk);
+        } else {
+          tandak[index] = [sailk];
+        }
+      });
+      // if (tanda1.length > 0) {
+      //   tandak.push(tanda1);
+      // }
+      // const tanda2 = sailkapena.filter((el) => el.tanda === 2);
+      // if (tanda2.length > 0) {
+      //   tandak.push(tanda2);
+      // }
+      // const tanda3 = sailkapena.filter((el) => el.tanda === 3);
+      // if (tanda3.length > 0) {
+      //   tandak.push(tanda3);
+      // }
+      this.estropada = {
+        izena: estropada.izena,
+        lekua: estropada.lekua,
+        data: estropada.data,
+        tandak: tandak,
+        sailkapena: sailkapena,
+        oharrak: estropada.oharrak
+      };
+    });
+  }
+}
