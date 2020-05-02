56de912bb1c4ab8f35a517ad3184d517cbc67e3d
Ander Garmendia <kelertxiki@gmail.com>
2020-05-02 16:25:47 +0200

    Fix endpoint

--- src/app/shared/taldeak.service.ts
+++ src/app/shared/taldeak.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 
@@ -20,16 +19,14 @@ export class TaldeakService {
         year: '' + year
       };
       return this.http.get(endpoint, {params});
-          //.pipe(map(res => res.json()));
   }
 
   getOne(team: string, league: string, year: number): Observable<any> {
-      const endpoint = `${estropadakUrl}taldeak/${team}/plantilla`;
+      const endpoint = `${estropadakUrl}taldeak/${team}`;
       const params = {
         league,
         year: '' + year
       };
       return this.http.get(endpoint, {params});
-          //.pipe(map(res => res.json()));
   }
 }
