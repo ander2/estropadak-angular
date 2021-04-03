1aa93a1a1f1f4373955869ee1564bd4b7033b29b
Ander Garmendia <kelertxiki@gmail.com>
2021-04-03 17:42:51 +0200

    Use active year in left menu links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UrteakService } from './shared/estropada.service';
 
 
 @Component({
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Estropadak.net';
+  activeYear = new Date().getFullYear();
+
+  constructor(private urteakService: UrteakService) {}
+
+  ngOnInit() {
+    this.urteakService.getOne('active_year')
+    .subscribe((res) => {
+      this.activeYear = res;
+    });
+  }
 }
