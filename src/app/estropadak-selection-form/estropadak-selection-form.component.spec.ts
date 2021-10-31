41b094ffdc311ddc337bc313127ae7a199ff9eb3
Ander Garmendia <kelertxiki@gmail.com>
2021-10-31 21:00:50 +0100

    Fix test stub

--- src/app/estropadak-selection-form/estropadak-selection-form.component.spec.ts
+++ src/app/estropadak-selection-form/estropadak-selection-form.component.spec.ts
@@ -5,13 +5,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-import { of } from 'rxjs';
-
 import { UrteakService, EstropadaService } from '../shared/estropada.service';
 import { EstropadakSelectionFormComponent } from './estropadak-selection-form.component';
 import { TaldeakService } from '../shared/taldeak.service';
 import { TaldeakServiceStub } from '../shared/taldeak.service.stub';
-import { EstropadaServiceStub } from 'app/shared/estropada.service.stub';
+import { EstropadaServiceStub, UrteakServiceStub } from '../shared/estropada.service.stub';
 
 describe('EstropadakSelectionFormComponent', () => {
   let component: EstropadakSelectionFormComponent;
@@ -31,7 +29,7 @@ describe('EstropadakSelectionFormComponent', () => {
       providers: [
         { provide: EstropadaService, useClass: EstropadaServiceStub },
         { provide: TaldeakService, useClass: TaldeakServiceStub },
-        { provide: UrteakService, useValue: {getList: () => of({'act': [2019]})} }
+        { provide: UrteakService, useClass: UrteakServiceStub }
       ]
     })
     .compileComponents();
